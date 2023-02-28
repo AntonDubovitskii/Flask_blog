47aeb70a7da2330f829c8f33c35b34a709e51782
AntonDubovitskii <gtoll@yandex.ru>
2023-02-28 13:07:03 +0400

    Homework3 - problem

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from blog.app import create_app
+from blog.app import create_app, db
 from flask import render_template
 
 app = create_app()
@@ -7,3 +7,22 @@ app = create_app()
 @app.route('/')
 def start():
     return render_template("index.html")
+
+
+@app.cli.command("init-db")
+def init_db():
+    db.create_all()
+    print("Done!")
+
+
+@app.cli.command("create-users")
+def create_users():
+    from blog.models import User
+    admin = User(username="admin", is_staff=True)
+    sam = User(username="sam")
+
+    db.session.add(admin)
+    db.session.add(sam)
+    db.session.commit()
+
+    print("Done! Created users:", admin, sam)
