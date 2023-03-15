3f51d9edaddc4e13bc796ebe44c9e322d6fcb774
AntonDubovitskii <96079388+AntonDubovitskii@users.noreply.github.com>
2023-03-15 13:41:42 +0400

    Homework7 - Done (#7)
    * Homework7 - Done
    
    * Homework7 - Done (added tag links)

--- blog/views/articles.py
+++ blog/views/articles.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, render_template, request, current_app, redirect, url_for
 from flask_login import login_required, current_user
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import joinedload
 from werkzeug.exceptions import NotFound
 
 from blog.extensions import db
 from blog.forms.article import CreateArticleForm
-from blog.models import Author, Article
+from blog.models import Author, Article, Tag
 
 articles_app = Blueprint("articles_app", __name__)
 
@@ -16,9 +17,15 @@ def articles_list():
     return render_template("articles/list.html", articles=articles)
 
 
+@articles_app.route("/tag/<int:tag_id>/", endpoint="tag")
+def articles_list(tag_id: int):
+    articles = Article.query.filter(Article.tags.any(Tag.id == tag_id))
+    return render_template("articles/list.html", articles=articles)
+
+
 @articles_app.route("/<int:article_id>/", endpoint="details")
 def article_details(article_id: int):
-    article = Article.query.filter_by(id=article_id).one_or_none()
+    article = Article.query.filter_by(id=article_id).options(joinedload(Article.tags)).one_or_none()
     if article is None:
         raise NotFound(f"User #{article_id} doesn't exist!")
     return render_template('articles/details.html', article=article)
@@ -29,12 +36,19 @@ def article_details(article_id: int):
 def create_article():
     error = None
     form = CreateArticleForm(request.form)
+    form.tags.choices = [(tag.id, tag.name) for tag in Tag.query.order_by("name")]
+
     if request.method == "POST" and form.validate_on_submit():
         _article = Article(title=form.title.data.strip(), body=form.body.data)
         if not current_user.author:
             author = Author(user_id=current_user.id)
             db.session.add(author)
             db.session.commit()
+
+        if form.tags.data:
+            selected_tags = Tag.query.filter(Tag.id.in_(form.tags.data))
+            for tag in selected_tags:
+                _article.tags.append(tag)
 
         _article.author = current_user.author
         db.session.add(_article)
