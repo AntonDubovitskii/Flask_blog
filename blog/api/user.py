83a2c3650b229ae2c7afe8e18919bafc665e07ea
AntonDubovitskii <96079388+AntonDubovitskii@users.noreply.github.com>
2023-03-22 18:52:44 +0400

    Homework9 - Done (#9)

---
+++ blog/api/user.py
@@ -0,0 +1,21 @@
+from flask_combo_jsonapi import ResourceDetail, ResourceList
+
+from blog.extensions import db
+from blog.schemas import UserSchema
+from blog.models import User
+
+
+class UserList(ResourceList):
+    schema = UserSchema
+    data_layer = {
+        "session": db.session,
+        "model": User,
+    }
+
+
+class UserDetail(ResourceDetail):
+    schema = UserSchema
+    data_layer = {
+        "session": db.session,
+        "model": User,
+    }
--- blog/extensions.py
+++ blog/extensions.py
@@ -1,7 +1,9 @@
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_combo_jsonapi import Api
 
 login_manager = LoginManager()
 db = SQLAlchemy()
 migrate = Migrate()
+api = Api()
