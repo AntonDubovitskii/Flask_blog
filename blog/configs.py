b58b44d1f2bcbd072163f542e2f519a8fdbfcadb
AntonDubovitskii <gtoll@yandex.ru>
2023-03-25 13:27:47 +0400

    template fix

--- blog/configs.py
+++ blog/configs.py
@@ -6,6 +6,7 @@ class BaseConfig(object):
     TESTING = False
     SQLALCHEMY_DATABASE_URI = "sqlite:///db.sqlite"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get("SECRET_KEY")
     WTF_CSRF_ENABLED = True
     FLASK_ADMIN_SWATCH = 'cosmo'
 
